991652aeacdf40c8038efad68c58d5265bef736f
bekreth <bekreth@proton.me>
2025-09-13 13:40:07 -0700

    added font and built main page

--- src/home_anchor.js
+++ src/home_anchor.js
@@ -7,8 +7,8 @@ class HomeAnchor extends HTMLElement {
 		const link = this.getAttribute("link");
 
 		this.outerHTML = `
-		<div id="home_anchor">
-			<a href="/${link}">
+		<div class="home_anchor">
+			<a href="/${link}" alt="bob" data-title="roy">
 				<img src="/icons/${link}.png"/>
 			</a>
 		</div>
